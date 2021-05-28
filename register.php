9ce9ea135157d944da74d76285a87ba7c051bac3
sonia <s.rolland@codeur.online>
2021-05-28 14:48:59 +0200

    udapte

--- register.php
+++ register.php
@@ -7,11 +7,16 @@ $email = $_POST['email'];
 $password = $_POST['password'];
 $confirmation = $_POST['confirmation'];
 
-
-if($password === $confirmation){
-    $password_encrypt =password_hash ($password, PASSWORD_DEFAULT);
-    $confirmation_encrypt =password_hash ($confirmation, PASSWORD_DEFAULT);
-    echo $username. '<br>' . $email. '<br>'.$password_encrypt.'<br>'. $confirmation_encrypt;
+if ($password === $confirmation) {
+      $passwordhash = password_hash($_POST['password'], PASSWORD_DEFAULT);
+      // faire requête sql
+      $sql = "INSERT INTO users(username,email,password) VALUES(:username,:email,:password)";
+      $query = $db->prepare($sql);
+      $query->bindValue(':username', $username, PDO::PARAM_STR);
+      $query->bindValue(':email', $email, PDO::PARAM_STR);
+      $query->bindValue(':password', $passwordhash, PDO::PARAM_STR);
+      $query->execute();
+      echo 'succes';
 }else{
-    echo 'les mots de passe ne correspondent pas';
+      echo 'les mots de passe ne correspondent pas';
 }
