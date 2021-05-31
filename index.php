4645c3b48cb85457b1146dd788c459fc86add108
sonia <s.rolland@codeur.online>
2021-05-31 11:59:46 +0200

    udapte

--- index.php
+++ index.php
@@ -1,35 +1,14 @@
 <!DOCTYPE html>
-<html lang="fr">
+<html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Backoffice</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>backoffice-portefolio</title>
 </head>
 <body>
-    <form action="register.php" method="post">
-<div>
-<label for="input-username">Nom d'utilisateur : </label>
-<input type="text" name="username" id="input-username">
-</div>
-<div>
-<label for="input-email">Email d'utilisateur : </label>
-<input type="email" name="email" id="input-email">
-</div>
-<div>
-<label for="input-password">Mot de passe d'utilisateur : </label>
-<input type="password" name="password" id="input-password">
-</div>
-<div>
-<label for="input-confirmation">Confirmer le mot de passe : </label>
-<input type="password" name="confirmation" id="input-confirmation" required>
-<span id='error'></span>
-</div>
-<div>
-   <input type="submit" value="enregistrer" id="submit">
-</div>
-
-</form>
-  <script src="main.js"></script>
+  
+    <a href="register-form.php">s'inscrire</a>
+    <a href="login-form.php">se connecter</a>
 </body>
 </html>
---
+++ register-form-handler.php
@@ -0,0 +1,28 @@
+<?php
+require_once('db-connect.php');
+if (isset($_POST['username']) && !empty($_POST['username']) && isset($_POST['email']) && !empty($_POST['email']) && isset($_POST['password']) && !empty($_POST['password']) && isset($_POST['confirmation']) && !empty($_POST['confirmation'])){
+      $username = strip_tags( $_POST['username']);
+      $email = strip_tags($_POST['email']);
+      $password = strip_tags($_POST['password']);
+      $confirmation = strip_tags($_POST['confirmation']);
+      
+      if ($password === $confirmation) {
+            $passwordhash = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+            $sql = "INSERT INTO users(username,email,password) VALUES(:username,:email,:password)";
+            $query = $db->prepare($sql);
+            $query->bindValue(':username', $username, PDO::PARAM_STR);
+            $query->bindValue(':email', $email, PDO::PARAM_STR);
+            $query->bindValue(':password', $passwordhash, PDO::PARAM_STR);
+            $query->execute();
+            echo 'succes';
+            echo '<br><a herf="index.php"><retour></a>';
+      }else{
+            echo 'les mots de passe ne correspondent pas';
+      }
+}else{
+   echo 'remplissez tout les champs du formulaire';   
+
+}
+
+
